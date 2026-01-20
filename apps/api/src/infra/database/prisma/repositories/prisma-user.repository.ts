23e48a297ae4278cb28ba03d45ce47957fcf2a5b
codesofC <inelusjudelin01@gmail.com>
2026-01-19 23:49:16 -0300

    #15 : feat(user): implement user entity, repository, and permission factory

---
+++ apps/api/src/infra/database/prisma/repositories/prisma-user.repository.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common'
+import { UserRepository } from '@/domain/master/application/repositories/user.repository'
+import { User } from '@/domain/master/entreprise/entities/user'
+import { PrismaUserMapper } from '../mappers/prisma-user.mapper'
+import { PrismaService } from '../prisma.service'
+
+@Injectable()
+export class PrismaUserRepository implements UserRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(entity: User): Promise<void> {
+    await this.prisma.user.create({
+      data: PrismaUserMapper.toPrisma(entity),
+    })
+  }
+
+  async findAll(): Promise<User[]> {
+    const users = await this.prisma.user.findMany()
+    return users.map(PrismaUserMapper.toDomain)
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.prisma.user.findUniqueOrThrow({
+      where: { id },
+    })
+    return PrismaUserMapper.toDomain(user)
+  }
+
+  async save(entity: User): Promise<void> {
+    await this.prisma.user.update({
+      where: { id: entity.id.toString() },
+      data: PrismaUserMapper.toPrisma(entity),
+    })
+  }
+
+  async delete(entity: User): Promise<void> {
+    await this.prisma.user.delete({
+      where: { id: entity.id.toString() },
+    })
+  }
+
+  async count(): Promise<number> {
+    return this.prisma.user.count()
+  }
+}
